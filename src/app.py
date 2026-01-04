7f558c328aa1961ec78a192fce9f94a0add5df1e
NugrahAnggaraS <bdaiunimed2024@gmil.com>
2026-01-04 17:36:01 +0700

    [Refactor][Add] change model and add scaler for transform input

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,jsonify
 import os
 import joblib
 import logging
+import numpy as np
 
 
 app = Flask(__name__)
@@ -11,24 +12,35 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(
     BASE_DIR,
     "model",
-    "LinearRegression_StudentPerformance.h5"
+    "linear_regression_model.pkl"
 )
+
+scaler_path = os.path.join(
+    BASE_DIR,
+    "model",
+    "scaler.pkl"
+)
+
 model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 @app.route("/predict",methods =['POST'])
 def predict_performance_student():
     payload = request.get_json()
 
     try:
-        X = [[
+        data = [
             float(payload.get("hour_studies")),
             float(payload.get("previous_scores")),
             int(payload.get("extracurricular_activities")),
             float(payload.get("sleep_hours")),
             int(payload.get("sample_question"))
-        ]]
-
-        result = model.predict(X)
+        ]
+        
+        X =  np.array(data).reshape(1,-1)
+        X_scaled = scaler.transform(X)
+        
+        result = model.predict(X_scaled)
         app.logger.info(f"Prediction result: {result}")
 
         return jsonify({
