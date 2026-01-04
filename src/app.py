47c13268fcb092e35073d918110dd44df0f6abb1
NugrahAnggaraS <bdaiunimed2024@gmil.com>
2026-01-04 16:00:53 +0700

    [Add] integration model for backend api

--- src/app.py
+++ src/app.py
@@ -1,13 +1,41 @@
 from flask import Flask,request,jsonify
+import os
+import joblib
+import logging
+
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(
+    BASE_DIR,
+    "model",
+    "LinearRegression_StudentPerformance.h5"
+)
+model = joblib.load(model_path)
 
 @app.route("/predict",methods =['POST'])
 def predict_performance_student():
     payload = request.get_json()
-    data1 = payload.get("data1")
-    data2 = payload.get("data2")
-    return jsonify({
-        "data1": data1,
-        "data2" :data2
-    })
+
+    try:
+        X = [[
+            float(payload.get("hour_studies")),
+            float(payload.get("previous_scores")),
+            int(payload.get("extracurricular_activities")),
+            float(payload.get("sleep_hours")),
+            int(payload.get("sample_question"))
+        ]]
+
+        result = model.predict(X)
+        app.logger.info(f"Prediction result: {result}")
+
+        return jsonify({
+            "prediction": float(result[0])
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 400
